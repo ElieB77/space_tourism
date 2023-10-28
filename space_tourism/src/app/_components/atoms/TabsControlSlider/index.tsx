4d381596aa1b073cd04dbd2efb45da1c104f0103
Elie Bissor <eliebiss.26@gmail.com>
2023-10-28 21:37:51 +0200

    Add TabsControlSlider component.

---
+++ space_tourism/src/app/_components/atoms/TabsControlSlider/index.tsx
@@ -0,0 +1,11 @@
+import "@/app/_styles/components/atoms/_tabs-control-slider.scss";
+
+export const TabsControlSlider = () => {
+  return (
+    <div className="__tabs_control_slider">
+      <p>moon</p>
+      <p>mars</p>
+      <p>jupiter</p>
+    </div>
+  );
+};
--- space_tourism/src/app/_components/pages/DestinationPage/index.tsx
+++ space_tourism/src/app/_components/pages/DestinationPage/index.tsx
@@ -1,15 +1,17 @@
+import { TabsControlSlider } from "../../atoms/TabsControlSlider";
 import { TwoTextBlock } from "../../atoms/TwoTextBlock";
 import data from "@/app/_assets/data.json";
 
 export const DestinationPage = () => {
   return (
     <div>
-      <TwoTextBlock
+      {/* <TwoTextBlock
         firstHeader={<h2>{data.destinations[0]["avg-distance"]}</h2>}
         secondHeader={<h2>{data.destinations[0]["est-travel"]}</h2>}
         firstContent={<h5>{data.destinations[1].distance}</h5>}
         secondContent={<h5>{data.destinations[0].travel}</h5>}
-      />
+      /> */}
+      <TabsControlSlider />
     </div>
   );
 };
